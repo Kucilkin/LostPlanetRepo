c80b194e99d8902b44a3aae3cb860fceceb77576
Moritz <moritz@doerdelmann.com>
2021-07-26 16:51:13 +0200

    PlayerControllerUpdate

--- Lost Planet/Assets/Scripts/PlayerController.cs
+++ Lost Planet/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float Speed; // Playerspeed
+    private float xInput; // Spielereingabe
+    private float towardsY = 0f; // Winkel zu dem dem sich der PLayer um eigene Achse dreht
+    
+    public float jumpPush = 5f; // Sprungkraft
+    private bool onGround = false;
+
+    public Rigidbody2D RB;
+    public GameObject playerModel;
+
     void Start()
     {
-        
+        RB = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        xInput = Input.GetAxisRaw("Horizontal"); //Eingabesignal fürs laufen
+
+        //Drehen:
+        //if (xInput > 0f) // nach rechts gehen
+        //    towardsY = 0f;
+        //else if (xInput < 0f) // nach links gehen
+        //    towardsY = 180f;
+
+        //playerModel.transform.rotation = Quaternion.Lerp(playerModel.transform.rotation, Quaternion.Euler(0f, towardsY, 0f), Time.deltaTime * 10f);
+
+        //Springen:
+        //RaycastHit2D hit2D;
+        //onGround = Physics.Raycast(transform.position (Vector3.up * 0, 1f), Vector3.down, out hit2D, 0,1f);
+
+        if (Input.GetAxisRaw("Jump") > 0f)
+        {
+            Vector2 JumpPower = RB.velocity;
+            JumpPower.y = jumpPush;
+            RB.velocity = JumpPower;
+        }
         
+
+    }
+
+    private void FixedUpdate()
+    {
+        RB.velocity = new Vector2(xInput * Speed, RB.velocity.y); //Vorfärtsbewegung
     }
 }
